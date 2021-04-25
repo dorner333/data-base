0cb20fcaefd007bc075b57cf74de4bb6d1e373a4
pavel-collab <rubikpf2002@mail.ru>
2021-04-25 23:09:02 +0300

    автоматический сбор данных

--- script.sh
+++ script.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/env bash
 
 echo "test"
-# передаем управление файлу с командами commans.txt
-for ((i = 0; i <= 10; i++))
+
+# отчищаем файл с данными
+rm data.txt
+
+# запускаем цикл
+for ((i = 1; i <= 10; i++))
 do
+# пишем команды в файл commands.txt
+printf "MASS 0 $i\nGET key$i\nDEL key$i\n" > commands.txt
+# передаем управление файлу с командами commans.txt
 ./hashdbtest new < commands.txt
-done 
+done
+
+# удаляем файл с базой
 rm new
 echo "end"
